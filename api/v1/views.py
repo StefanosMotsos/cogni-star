dc0bb70512c1da306b44a72ff45f028986ae43ca
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-08-24 12:05:24 +0300

    feat: add pagination

--- api/v1/views.py
+++ api/v1/views.py
@@ -2,16 +2,19 @@ from rest_framework.authtoken.admin import User
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.viewsets import ModelViewSet
 from api.models import Planet, Person
+from api.v1.pagination import SmallSetPagination
 from api.v1.serializers import PlanetSerializer, PersonSerializer, UserSerializer
 
 
 class PlanetViewSet(ModelViewSet):
     queryset = Planet.objects.all()
     serializer_class = PlanetSerializer
+    pagination_class = SmallSetPagination
 
 class PersonViewSet(ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    pagination_class = SmallSetPagination
 
 class UserDetailsAPIView(RetrieveUpdateAPIView):
     queryset = User.objects.all()

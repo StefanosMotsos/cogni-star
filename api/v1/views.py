eaf98219f1fa71fb958152c0dc6cd96d896a2e0f
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-08-24 12:27:11 +0300

    feat: add permissions

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,8 +1,10 @@
 from rest_framework.authtoken.admin import User
 from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 from api.models import Planet, Person
 from api.v1.pagination import SmallSetPagination
+from api.v1.permissions import IsOwnerOrAdmin
 from api.v1.serializers import PlanetSerializer, PersonSerializer, UserSerializer
 
 
@@ -11,14 +13,29 @@ class PlanetViewSet(ModelViewSet):
     serializer_class = PlanetSerializer
     pagination_class = SmallSetPagination
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAuthenticated, IsAdminUser]
+        return [permission() for permission in permission_classes]
+
 class PersonViewSet(ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
     pagination_class = SmallSetPagination
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAuthenticated, IsAdminUser]
+        return [permission() for permission in permission_classes]
+
 class UserDetailsAPIView(RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
 
     def get_object(self):
         return self.request.user

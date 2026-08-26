681f575477dc547349509731a5f0d0098fc08620
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-08-26 11:17:35 +0300

    feat: add populate view for swapi

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.v1.views import PersonViewSet, PlanetViewSet, UserDetailsAPIView
+from api.v1.views import PersonViewSet, PlanetViewSet, UserDetailsAPIView, PopulateView
 
 router = DefaultRouter()
 router.register('people', PersonViewSet)
@@ -10,4 +10,5 @@ router.register('planets', PlanetViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('me/', UserDetailsAPIView.as_view()),
+    path('populate/', PopulateView.as_view()),
 ]
